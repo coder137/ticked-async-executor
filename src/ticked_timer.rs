6a036ec797de89973b1f01c579cb7a03bb4e6fb2
Niket Naidu <coder137@gmail.com>
2024-07-17 20:28:41 -0700

    TickedTimer implementation
    - TickedExecutor now takes delta_in_ms and limit as parameters

---
+++ src/ticked_timer.rs
@@ -0,0 +1,19 @@
+pub struct TickedTimer {
+    pub tick_recv: tokio::sync::watch::Receiver<f64>,
+}
+
+impl TickedTimer {
+    pub async fn sleep_for(mut self, mut duration_in_ms: f64) {
+        loop {
+            let _r = self.tick_recv.changed().await;
+            if _r.is_err() {
+                break;
+            }
+            let current_dt = *self.tick_recv.borrow_and_update();
+            duration_in_ms -= current_dt;
+            if duration_in_ms <= 0.0 {
+                break;
+            }
+        }
+    }
+}
